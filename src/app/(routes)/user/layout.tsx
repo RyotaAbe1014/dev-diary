2cadc9c549a8a9c18cd248e2e77e8bc1cab04e84
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-19 00:37:19 +0900

    ✨ feat: zlayout.tsxに作成したヘッダーを追加

--- src/app/(routes)/user/layout.tsx
+++ src/app/(routes)/user/layout.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import { Header } from "@/features/user/components/Header/header";
 
 export default function Layout({
   children,
@@ -7,19 +7,7 @@ export default function Layout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="border-b">
-        <div className="container py-4 md:py-6">
-          <div className="flex items-center gap-4">
-            <h1 className="text-2xl font-bold tracking-tighter">Home</h1>
-            <Link
-              className="ml-auto inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50"
-              href="#"
-            >
-              New Article
-            </Link>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="flex-1 py-4 md:py-6">
         {children}
       </main>
