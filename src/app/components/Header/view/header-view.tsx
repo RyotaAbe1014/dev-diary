35c63bcfa39ad2483dfeea2be20bfe27cd378b3a
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-21 10:42:08 +0900

    💄

--- src/app/components/Header/view/header-view.tsx
+++ src/app/components/Header/view/header-view.tsx
@@ -1,14 +1,6 @@
 'use client';
-
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuItem
-} from "@/lib/components/ui/dropdown-menu";
 import Link from "next/link";
+import { ActionMenu } from "../../ActionMenu/action-menu";
 
 type Props = {
   pathName: string;
@@ -22,19 +14,7 @@ export function HeaderView({ pathName, isLogin }: Props) {
         <div className="flex items-center gap-4">
           <h1 className="text-2xl font-bold tracking-tighter">{pathName}</h1>
           {isLogin ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger className="ml-auto inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50">
-                Menu
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <ActionMenu />
           ) : (
             <Link
               className="ml-auto inline-flex h-10 items-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50"
