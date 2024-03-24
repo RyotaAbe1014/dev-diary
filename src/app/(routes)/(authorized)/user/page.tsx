c67d6fbfe2e837e3b2e2358b09892622671dae02
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-24 22:50:31 +0900

    🩹 fix: Linkコンポーネントがtext要素につけてしまっていたのでカードをリンクでラップするように修正

--- src/app/(routes)/(authorized)/user/page.tsx
+++ src/app/(routes)/(authorized)/user/page.tsx
@@ -2,26 +2,27 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-
     <div className="container grid max-w-6xl gap-4 px-4 md:px-6">
       <div className="grid grid-cols-2 gap-4">
-        <div className="flex flex-col gap-4 p-4 rounded-lg border hover:bg-gray-100 dark:hover:bg-gray-800">
-          <Link className="text-xl font-semibold" href="#">
-            User Menu
-          </Link>
-          <Link className="text-sm text-gray-500 dark:text-gray-400" href="#">
-            User menu description or actions
-          </Link>
-        </div>
-        <div className="flex flex-col gap-4 p-4 rounded-lg border hover:bg-gray-100 dark:hover:bg-gray-800">
-          <Link href="/user/article">
+        <Link href="#">
+          <div className="flex flex-col gap-4 p-4 rounded-lg border hover:bg-gray-100 dark:hover:bg-gray-800">
+            <p className="text-xl font-semibold">
+              User Menu
+            </p>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              User menu description or actions
+            </p>
+          </div>
+        </Link>
+        <Link href="/user/article">
+          <div className="flex flex-col gap-4 p-4 rounded-lg border hover:bg-gray-100 dark:hover:bg-gray-800">
             <p className="text-xl font-semibold" >Article Menu</p>
             <p className="text-sm text-gray-500 dark:text-gray-400">
               Article menu description or actions
             </p>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
-    </div>
+    </div >
   );
 }
