55643cbc72591d32cd4f12f737bc79d556e60deb
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-25 23:24:32 +0900

    ✨ feat: シンタックスハイライトの適用

--- src/features/article/components/ArticleMarkDownEditor/article-mark-down-editor.tsx
+++ src/features/article/components/ArticleMarkDownEditor/article-mark-down-editor.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import React from 'react'
-import Markdown from 'react-markdown'
+import React from 'react';
+import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export function ArticleMarkDownEditor() {
   const text = `
@@ -19,12 +21,36 @@ export function ArticleMarkDownEditor() {
 
   | a | b |
   | - | - |
+  ~~~python
+  i = 0
+
+  for i in range(10):
+    print(i)
+  ~~~
   `
   return (
     <div>
-      <Markdown remarkPlugins={[remarkGfm]} className='markdown'>
-        {text}
-      </Markdown>
+      <Markdown
+        children={text}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+              <SyntaxHighlighter
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={materialDark}
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      />
     </div >
   )
 }
