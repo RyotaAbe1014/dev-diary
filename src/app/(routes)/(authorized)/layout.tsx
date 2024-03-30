6af8c615c1a471f87a09e057ccb9b6c54b54678e
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-30 22:11:53 +0900

    🛂 feat: add auth

--- src/app/(routes)/(authorized)/layout.tsx
+++ src/app/(routes)/(authorized)/layout.tsx
@@ -1,11 +1,15 @@
 import { cookies } from "next/headers";
 import { Header } from "../../components/Header/header"
+import { redirect } from "next/navigation";
 export default function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
   const token = cookies().get('token')?.value
+  if (!token) {
+    redirect('/auth/login');
+  }
   return (
     <div className="flex flex-col min-h-screen">
       <Header isLogin={!!token} />
