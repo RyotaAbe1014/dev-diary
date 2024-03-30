37ccc5da4613f4c591da60e487fce0a2b1c17a5b
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-30 22:14:04 +0900

    ✅ test: add action menu test

--- src/app/components/ActionMenu/action-menu.test.tsx
+++ src/app/components/ActionMenu/action-menu.test.tsx
@@ -1,8 +1,21 @@
 import { render, screen } from "@testing-library/react";
-import { ActionMenu } from "./action-menu";
+import { expect, test, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
+import { ActionMenu } from "./action-menu";
+import * as LoginModule from "@/features/auth/actions/logout";
+import { logout } from "@/features/auth/actions/logout";
 
 const user = userEvent;
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+vi.mock('@/features/auth/actions/logout');
+
+function mockLogout() {
+  vi.spyOn(LoginModule, 'logout').mockResolvedValue();
+}
 
 describe("ActionMenu", () => {
   test("Menuボタンが表示される", () => {
@@ -28,5 +41,20 @@ describe("ActionMenu", () => {
     // then
     const menuContent = screen.getByRole("menu");
     expect(menuContent).toBeInTheDocument();
+  });
+
+  test('logoutボタンをクリックするとlogout関数が呼ばれる', async () => {
+    // given
+    mockLogout();
+    render(<ActionMenu />);
+    const menuTrigger = screen.getByRole("button", { name: "Menu" });
+
+    // when
+    await user.click(menuTrigger);
+    const logoutButton = screen.getByRole("menuitem", { name: "logout" });
+    await user.click(logoutButton);
+
+    // then
+    expect(logout).toHaveBeenCalled();
   });
 });
