6de299e6b19ab0bc6f9445937b552636305e3fef
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-03-31 18:56:02 +0900

    ♻️ refactor: 処理のリファクタ

--- src/features/article/components/ArticleMarkDownEditor/article-mark-down-editor.tsx
+++ src/features/article/components/ArticleMarkDownEditor/article-mark-down-editor.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import React from 'react';
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Textarea } from '@/lib/components/ui/textarea';
+import { MarkDown } from '@/lib/react-markdown/components/MarkDown/MarkDown';
 
 export type ArticleMarkDownEditorProps = {
   value: string;
@@ -14,31 +11,9 @@ export type ArticleMarkDownEditorProps = {
 
 export function ArticleMarkDownEditor({ value, onChange }: ArticleMarkDownEditorProps) {
   return (
-    <div className='flex gap-4 h-[calc(100vh-4rem)]'>
+    <div className='flex gap-4 h-max'>
       <Textarea value={value} onChange={(e) => onChange(e.target.value)} className='resize-none w-1/2' placeholder='Write something here...' />
-      <Markdown
-        children={value}
-        className='markdown w-1/2 p-4 rounded-lg border border-slate-200 dark:border-slate-800 overflow-y-auto overflow-x-auto'
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code(props) {
-            const { children, className, node, ...rest } = props
-            const match = /language-(\w+)/.exec(className || '')
-            return match ? (
-              <SyntaxHighlighter
-                PreTag="div"
-                children={String(children).replace(/\n$/, '')}
-                language={match[1]}
-                style={materialDark}
-              />
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            )
-          }
-        }}
-      />
+      <MarkDown value={value} className='w-1/2 p-2 rounded-lg border border-slate-200 dark:border-slate-800 overflow-y-auto overflow-x-auto' />
     </div >
   )
 }
