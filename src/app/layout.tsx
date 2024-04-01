1a3b96876c4b27f8c451c043da7eb90cd64db654
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-01 23:42:23 +0900

    ✨ feat: add GlobalSpinner in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { GlobalSpinner } from "@/utils/Loader/GlobalSpinner/GlobalSpinner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <GlobalSpinner />
       </body>
     </html>
   );
