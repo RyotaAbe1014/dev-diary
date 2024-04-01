53b16e5b2346009bc256dd01a4982ba889736f9f
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-01 23:47:30 +0900

    ✨ feat: show GlobalSpinner

--- src/utils/Loader/GlobalSpinner/GlobalSpinner.tsx
+++ src/utils/Loader/GlobalSpinner/GlobalSpinner.tsx
@@ -1,10 +1,13 @@
-import { Loader2 } from "lucide-react";
+'use client';
+
+import React from 'react';
+import { useAtom } from 'jotai';
+import { GlobalSpinnerView } from './view/GlobalSpinnerView';
+import { isShowGlobalSpinnerAtom } from './globalSpinnerAtom';
 
 export function GlobalSpinner() {
+  const [isShowGlobalSpinner] = useAtom(isShowGlobalSpinnerAtom);
   return (
-    // 背景色を透明なグレーにして、ローディングアイコンを中央に配置する
-    <div className="w-full h-full flex justify-center items-center bg-gray-200 bg-opacity-50 fixed top-0 left-0 z-50">
-      <Loader2 className="h-28 w-28 animate-spin" />
-    </div>
+    isShowGlobalSpinner ? <GlobalSpinnerView /> : null
   );
 }
