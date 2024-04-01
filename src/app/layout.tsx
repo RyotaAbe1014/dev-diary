21187d339f53cdc43d97a153cbf9843598699322
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-01 23:48:38 +0900

    🚚 chore: move files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import { GlobalSpinner } from "@/utils/Loader/GlobalSpinner/GlobalSpinner";
+import { GlobalSpinner } from "@/utils/Loader/components/GlobalSpinner/GlobalSpinner";
 
 const inter = Inter({ subsets: ["latin"] });
 
