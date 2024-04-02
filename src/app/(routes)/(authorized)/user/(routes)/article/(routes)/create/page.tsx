f7cfb73af3eef720dd5c74cdd0d9c92aa0a3c080
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-02 23:43:22 +0900

    ✨feat: add create page

---
+++ src/app/(routes)/(authorized)/user/(routes)/article/(routes)/create/page.tsx
@@ -0,0 +1,9 @@
+import { UserArticleDetail } from "@/features/user/components/UserArticleDetail/UserArticleDetail";
+
+export default function Page() {
+  return (
+    <div>
+      create page
+    </div>
+  );
+}
