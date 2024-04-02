400e64283c738314ac1ed7b07bd799981a784975
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-03 01:03:14 +0900

    ♻️ style: delete debug code

--- src/app/(routes)/(authorized)/user/(routes)/article/(routes)/create/page.tsx
+++ src/app/(routes)/(authorized)/user/(routes)/article/(routes)/create/page.tsx
@@ -1,7 +1,6 @@
 import { UserArticleCreate } from "@/features/user/components/userArticleCreate/UserArticleCreate";
 
 export default function Page() {
-  console.log("Page: src/app/%28routes%29/%28authorized%29/user/%28routes%29/article/%28routes%29/create/page.tsx");
   return (
     <div className="container grid max-w-6xl gap-4 px-4 md:px-6">
       <UserArticleCreate />
