f96e89cab140e157062a44389b7dc5d612325f12
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-08 22:32:49 +0900

    style:  delete unused next/headers

--- src/app/(routes)/(authorized)/layout.tsx
+++ src/app/(routes)/(authorized)/layout.tsx
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import { Header } from "../../components/Header/header"
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
