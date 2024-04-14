99c88cecb5081e1cf6e7cdfbfdd559fd8a3c0a2e
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-14 18:15:36 +0900

    ✨ feat: add delete func

--- src/features/article/actions/index.ts
+++ src/features/article/actions/index.ts
@@ -1,9 +1,11 @@
 'use server';
+import { redirect } from "next/navigation";
+import { revalidateTag } from "next/cache";
 
 import { logout } from "@/features/auth/actions/logout";
 import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
 import { userArticleSchema } from "../schemas/userArticleSchema";
+import { UserArticle } from "../types/UserArticle";
 
 export async function createArticle({ title, description, text }: { title: string, description: string, text: string }) {
   const result = userArticleSchema.safeParse({ title, description, text });
@@ -36,6 +38,35 @@ export async function createArticle({ title, description, text }: { title: strin
   if (error) {
     return [error.message];
   }
-
+  revalidateTag('userArticleList');
   redirect('/user/article');
-}
+};
+
+export async function getArticleList() {
+  const supabase = createClient();
+  const userResponse = await supabase.auth.getUser();
+  const user = userResponse.data.user;
+
+  if (!user) {
+    await logout();
+    return;
+  }
+
+  const { data, error } = await supabase.from('articles').select('*').eq('user_id', user.id);
+
+  if (error || data === null) {
+    throw error;
+  }
+
+  return data as UserArticle[];
+};
+
+export async function deleteArticle(articleId: number) {
+  const supabase = createClient();
+  const { error } = await supabase.from('articles').delete().eq('id', articleId);
+
+  if (error) {
+    return [error.message];
+  }
+  revalidateTag('userArticleList');
+};
