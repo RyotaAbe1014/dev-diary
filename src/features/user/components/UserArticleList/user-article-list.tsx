58847fd913fb00231984db51484d7d78b68d8112
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-15 23:48:23 +0900

    🐛 fix: unstable_cache　error

--- src/features/user/components/UserArticleList/user-article-list.tsx
+++ src/features/user/components/UserArticleList/user-article-list.tsx
@@ -1,13 +1,9 @@
 import { getArticleList } from "@/features/article/actions";
 import { UserArticleListView } from "./view/user-article-list-view";
 import { camelizeDeeply } from "@/utils/camelizeDeeply/camelizeDeeply";
-import { unstable_cache } from "next/cache";
-
-
-const cachedUserArticleList = unstable_cache(getArticleList, ['userArticleList'], { tags: ['userArticleList'], revalidate: 1});
 
 export async function UserArticleList() {
-  const data = await cachedUserArticleList();
+  const data = await getArticleList();
   if (!data) {
     return null;
   }
