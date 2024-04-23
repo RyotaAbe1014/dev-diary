4c036909efab6b1d9afac7d703332194473e2175
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-04-23 23:09:52 +0900

    feat: fix model

--- src/app/(routes)/(authorized)/user/(routes)/article/page.tsx
+++ src/app/(routes)/(authorized)/user/(routes)/article/page.tsx
@@ -1,6 +1,6 @@
+import { Suspense } from "react";
 import { UserArticleList } from "@/features/user/components/UserArticleList/user-article-list";
 import { GlobalSpinnerView } from "@/utils/Loader/components/GlobalSpinner/view/GlobalSpinnerView";
-import { Suspense } from "react";
 
 export default function Page() {
   return (
