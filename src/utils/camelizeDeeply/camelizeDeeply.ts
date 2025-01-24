c8caed2e0736d88077ee8eaf0b946d2719fa6b82
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-01-24 23:56:06 +0900

    feat: add DeepCamelCase type for improved type safety in camelizeDeeply

--- src/utils/camelizeDeeply/camelizeDeeply.ts
+++ src/utils/camelizeDeeply/camelizeDeeply.ts
@@ -2,3 +2,9 @@ import camelcaseKeys from 'camelcase-keys'
 
 export const camelizeDeeply = <T extends Record<string, any> | readonly any[]>(arg: T) =>
   camelcaseKeys(arg, { deep: true });
+
+
+
+type DeepCamelCase<T> = T extends Record<string, unknown> ? {
+  [K in keyof T as Uncapitalize<K & string>]: DeepCamelCase<T[K]>
+} : T;
